3c40d05df1f8c34a46990c9d13ac767bb8d65a03
marcin.milik <marcin.milik@lodgify.com>
2021-01-13 12:38:39 +0100

    Init

---
+++ DynamicLogTestService/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Extensions.Logging;
+
+namespace DynamicLogTestService
+{
+    public static class Names
+    {
+        public static string EnvName = "MY_POD_IP";
+    }
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                        .ConfigureLogging((c, lb) =>
+                        {
+                            lb.AddConsole();
+                            lb.AddDynamicConsole();
+                        })
+                        .UseKestrel();
+                });
+        }
+            
+    }
+}
---
+++ DynamicLogTestService/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Steeltoe.Management.Endpoint;
+using Steeltoe.Management.Endpoint.Health;
+using Steeltoe.Management.Endpoint.Hypermedia;
+using Steeltoe.Management.Endpoint.Info;
+using Steeltoe.Management.Endpoint.Loggers;
+
+namespace DynamicLogTestService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            
+            services.AddInfoActuator(Configuration);
+            services.AddHealthActuator(Configuration);
+            services.AddLoggersActuator(Configuration);
+            services.AddHypermediaActuator(Configuration);
+            
+            services.AddMvc();
+            
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                
+                endpoints.Map<InfoEndpoint>();
+                endpoints.Map<HealthEndpoint>();
+                endpoints.Map<LoggersEndpoint>();
+                endpoints.Map<ActuatorEndpoint>();
+            });
+        }
+    }
+}
